6b6875d4de44172af4cff97b6fd4085fb3360c97
notPlancha <andre.plancha@hotmail.com>
2022-09-16 22:48:08 +0100

    finished new render.R

---
+++ render.R
@@ -0,0 +1,23 @@
+# I made a new render to get rid of the hack
+library(here)
+toProcess <- c("tpc1")
+
+for (i in toProcess){
+  rmarkdown::render(
+    input = here(i, paste0(i, ".rmd")),
+    clean = FALSE,
+    run_pandoc = FALSE,
+    intermediates_dir = here("temp"),
+    quiet = TRUE
+  )
+
+  system2(
+    command = rmarkdown::pandoc_exec(),
+    args = paste(
+      "-s", here("temp", paste0(i, ".knit.md")),
+      "-o", here(i,      paste0(i, ".pdf")),
+      "--pdf-engine", paste0(tinytex::tinytex_root(), "\\bin\\win32\\pdflatex.exe")
+    )
+  )
+  print(paste("File outputed if no errors:", here(i,      paste0(i, ".pdf"))))
+}
--- renderOld.r
+++ renderOld.r
@@ -4,9 +4,10 @@ library(base)
 templateFile <- ("template/Eisvogel/eisvogel.latex")
 renderName <- ("tpc1")
 tempPath <- ("temp.rmd")
+keyword <- "TEMPLATEPATH"
 
 orig <- readLines(paste0(renderName, "/", renderName, ".rmd"))
-toTemp <- stringr::str_replace_all(orig, pattern = "TEMPLATEPATH", file_path_as_absolute(templateFile))
+toTemp <- stringr::str_replace_all(orig, pattern = keyword, file_path_as_absolute(templateFile))
 writeLines(toTemp, tempPath)
 
 rmarkdown::render(tempPath, clean = TRUE, output_file = paste0(renderName, "/", renderName, ".pdf"))
