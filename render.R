e2ccbaee5c811c7e77f8043bbf491eaedaebc5aa
notPlancha <andre.plancha@hotmail.com>
2022-09-21 23:16:43 +0100

    tpc3 até à 8

--- render.R
+++ render.R
@@ -1,17 +1,18 @@
 # I made a new render to get rid of the hack
 library(here)
 toProcess <- commandArgs(TRUE)
-if (length(toProcess) == 0) toProcess <- c("tpc1", "tpc2")
+if (length(toProcess) == 0) toProcess <- c("tpc1", "tpc2", "tpc3")
 
 for (i in toProcess){
   rmarkdown::render(
     input = here(i, paste0(i, ".rmd")),
     clean = FALSE,
     run_pandoc = FALSE,
-    intermediates_dir = here("temp"),
-    quiet = TRUE
-  )
+    quiet = TRUE,
+    intermediates_dir = here("temp")
 
+  )
+  print("render of md done")
   system2(
     command = rmarkdown::pandoc_exec(),
     args = paste(
@@ -19,8 +20,9 @@ for (i in toProcess){
       "-o", here(i,      paste0(i, ".pdf")),
       "--pdf-engine", paste0(tinytex::tinytex_root(), "\\bin\\win32\\pdflatex.exe"),
       "--template", here("template", "Eisvogel", "eisvogel.latex"),
-      "--listings"
+      "--listings",
+      "--resource-path", here(i)
     )
   )
-  print(paste("File outputed if no errors:", here(i,      paste0(i, ".pdf"))))
+  print(paste("File outputed if no errors:", here(i, paste0(i, ".pdf"))))
 }
