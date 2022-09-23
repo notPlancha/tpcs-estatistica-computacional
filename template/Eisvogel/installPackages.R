fcfda0a521a154455338604e8cfdfe30efd262ab
notPlancha <andre.plancha@hotmail.com>
2022-09-23 15:30:16 +0100

    tpc3 done

--- template/Eisvogel/installPackages.R
+++ template/Eisvogel/installPackages.R
@@ -1,2 +1,2 @@
-#tinytex::install_tinytex()
-tinytex::tlmgr_install("koma-script l3packages listings adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref")
+#tinytex::install_tinytex(bundle='TinyTeX')
+tinytex::tlmgr_install("l3packages listings adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref")
