8b62581c487fe4b8880dfd161034056f991890f9
notPlancha <andre.plancha@hotmail.com>
2022-10-02 20:56:39 +0100

    tpc05 feita

--- template/Eisvogel/installPackages.R
+++ template/Eisvogel/installPackages.R
@@ -1,3 +1,2 @@
 tinytex::uninstall_tinytex()
-tinytex::install_tinytex(bundle='TinyTeX')
-tinytex::tlmgr_install("comment l3packages listings adjustbox babel-german background bidi collectbox csquotes everypage filehook footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk xurl zref")
+tinytex::install_tinytex(bundle='TinyTeX-2')
